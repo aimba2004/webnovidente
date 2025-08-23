3b68b3a896a973cd7c5cfc7abb00744514d70277
aimba2004 <139301451+aimba2004@users.noreply.github.com>
2025-08-22 20:43:06 -0500

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Bienvenido Contacto de Emergencia</title>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+  <h2>Bienvenido contacto de emergencia</h2>
+  <p>Aquí podrás ver la ubicación del usuario y recibir notificaciones.</p>
+</body>
+</html>
